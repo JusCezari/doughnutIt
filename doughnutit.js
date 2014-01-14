759f3316d05d5d6e1fae0596b69ebbdc83cf4d47
JusCezari <juscezari@gmail.com>
2014-01-14 16:51:25 -0200

    Sketch of the texts done. Top lane of the right canvas working with some stuff todo

--- doughnutit.js
+++ doughnutit.js
@@ -26,7 +26,7 @@
 			// Style for the text inside
 			dnFontStyle: 'normal',			
 			// Size in px for the text inside
-			dnFontSize: 24,
+			dnFontSize: '190px',
 			// Color for the text inside
 			dnFontColor: "#666",
 			// Start angle for the first slice of the doughnut
@@ -88,57 +88,155 @@
         	// Create a CANVAS element inside the element
         	$('#'+wrapperId).append(rightCanvas);
 
-        	// These are the defaults values for the right CANVAS
+
+        	/* GENERIC STUFF */
+        	// These are the defaults values for the right CANVAS, both bottom and top lanes
 	        var rightSettings = $.extend({
 	        	// Radius of the sphere on the middle of the CANVAS
 	        	rcRadius: 5,
+	        	// Offset for sphere on the middle, normally the same value of the radius
+	        	rcOffset: 5,
+	        	// Color for the sphere
+	        	rcSphereColor: '#819596',
+	        	// Color of the sphere stroke
+				rcSphereStroke: '#819596',
 	        	// Distance of the margin on the end of the angle
 	        	rcPreMargin: 10,
 	        	// Distance before drawing the text TODO - Automatic calc
-	        	rcMargin: 10,
-	        	// Text to display
-	        	rcText: 'MÉDIA',
-
-	        	rcHeight: 100
+	        	rcMargin: 10,	        	
+	        	// Height of the line from the middle line
+	        	rcHeight: 100,
+	        	// Default value for the top lane
+	        	rcTop: false,
+	        	// Default value for the bottom lane
+	        	rcBottom: false
 	        }, settings.dnRightCanvas );
 
 
+	        // Just some debugging
+	        console.log("RIGHT SETTINGS");
 	        console.log(rightSettings);
 
-			var c=document.getElementById(rightCanvasId);
 
-		    var ctx = c.getContext('2d');
-		    var centerX = c.width / 2;
-		    var centerY = c.height / 2;
-		    var radius = 5;
 
-		    var offset = 5;
+	        // CIRCLE ON THE MIDDLE OF THE CANVAS STUFF
+	        if( (rightSettings.rcTop !== false) || (rightSettings.rcBottom !== false) ){
 
-		    ctx.beginPath();
-		    ctx.arc(offset+radius, centerY, radius, 0, 2 * Math.PI, false);
-		    ctx.fillStyle = '819596';
-		    ctx.fill();
-		    ctx.strokeStyle = '#819596';
-		  	ctx.stroke();
-		  	ctx.closePath();
-		    
-			
-			ctx.beginPath();
-			ctx.moveTo(10,c.height / 2);
-			ctx.lineTo(50,45);
-			ctx.stroke();
-			ctx.lineTo(200,45);
-			ctx.stroke();
-			ctx.closePath();
+				// Get the right CANVAS element
+        		var c=document.getElementById(rightCanvasId);
+        		// Get the context of the element
+			    var ctx = c.getContext('2d');
+			    // Place it on the center
+			    var centerX = c.width / 2;
+			    var centerY = c.height / 2;
+			    // Radius and offset
+			    var radius = rightSettings.rcRadius;
+			    var offset = rightSettings.rcOffset;
+			    // Draw the circle on the CANVAS
+			    ctx.beginPath();
+			    ctx.arc(offset+radius, centerY, radius, 0, 2 * Math.PI, false);
+			    ctx.fillStyle = rightSettings.rcSphereColor;
+			    ctx.fill();
+			    ctx.strokeStyle = rightSettings.rcSphereStroke;;
+			  	ctx.stroke();
+			  	ctx.closePath();
 
-			ctx.font="100% Arial";
-			ctx.fillText('MÉDIA', 149, 40);
+			  	/* TOP LANE STUFF */
+		        // If there is a top lane
+		        if(rightSettings.rcTop !== false){
 
-			ctx.font="300% Arial";
-			ctx.fillText('8.3', 135, 90);
+		        	/* GENERIC TOP LANE STUFF */
+	        		// These are the defaults values for TOP lane in right CANVAS
+			        var rcTopSettings = $.extend({
+			        	// Font family for the top lane
+			        	rcTopFontFamily: settings.dnFontFamily,
+			        	// Font size for the top lane
+			        	rcTopFontSize: settings.dnFontSize,
+			        	// Font color for the top lane
+			        	rcTopFontColor: settings.dnFontColor,
+			        	// Font style for the top lane
+			        	rcTopFontStyle: settings.dnFontStyle,
+			        	// Color of the line on top lane
+			        	rcTopLineColor: settings.dnFontColor,
+			        	// Object for above top lanve
+			        	rctAbove: false,
+			        	// Object for below top lanve
+			        	rctBelow: false
+			        }, rightSettings.rcTop );
+
+			        // Draw the LINES
+		        	ctx.beginPath();
+		        	ctx.strokeStyle = rcTopSettings.rcTopLineColor;
+					ctx.moveTo(10,c.height / 2);
+					ctx.lineTo(50,45);
+					ctx.stroke();
+					ctx.lineTo(200,45);
+					ctx.stroke();
+					ctx.closePath();
+					
+					/* ABOVE TOP LANE STUFF */
+					// If there are something to draw above the line
+			        if(rcTopSettings.rctAbove !== false){
+			        	
+		        		// These are the defaults values for ABOVE TOP lane in right CANVAS
+				        var rctSettings = $.extend({
+				        	// Font family for the top lane
+				        	rctFontFamily: rcTopSettings.rctFontFamily,
+				        	// Font size for the top lane
+				        	rctFontSize: rcTopSettings.rctFontSize,
+				        	// Font color for the top lane
+				        	rctFontColor: rcTopSettings.rctFontColor,
+				        	// Font style for the top lane
+				        	rctFontStyle: rcTopSettings.rctFontStyle,
+				        	// Text for above top lane
+				        	rctText: false,				        	
+				        }, rcTopSettings.rctAbove);
+
+						//Draw the TEXT ABOVE
+						ctx.fillStyle = rctSettings.rctFontColor;
+						ctx.font=rctSettings.rctFontStyle + " " + rctSettings.rctFontSize + " " + rctSettings.rctFontFamily;
+
+						// TODO - Calc the position where the text will be draw
+						ctx.fillText(rctSettings.rctText, 136, 40);
+						
+			        }
+
+
+			        /* BELOW TOP LANE STUFF */
+					// If there are something to draw below the line
+			        if(rcTopSettings.rctBelow !== false){
+			        	
+		        		// These are the defaults values for BELOW TOP lane in right CANVAS
+				        var rctSettings = $.extend({
+				        	// Font family for the top lane
+				        	rctFontFamily: rcTopSettings.rctFontFamily,
+				        	// Font size for the top lane
+				        	rctFontSize: rcTopSettings.rctFontSize,
+				        	// Font color for the top lane
+				        	rctFontColor: rcTopSettings.rctFontColor,
+				        	// Font style for the top lane
+				        	rctFontStyle: rcTopSettings.rctFontStyle,
+				        	// Text for above top lane
+				        	rctText: false,				        	
+				        }, rcTopSettings.rctBelow);
+
+						//Draw the TEXT BELOW
+						ctx.fillStyle = rctSettings.rctFontColor;
+						ctx.font=rctSettings.rctFontStyle + " " + rctSettings.rctFontSize + " " + rctSettings.rctFontFamily;
+
+						// TODO - Calc the position where the text will be draw
+						ctx.fillText(rctSettings.rctText, 136, 140);
+						
+			        }
+					
+		        }
+
+			}
 
 		}
 
     };
 
 }( jQuery ));
+
+// TODO - All default params follow through the hierarchy
